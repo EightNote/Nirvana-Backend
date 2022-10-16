cfdae6f578b33cb0776863ff59438e69c669fb3c
vivekKr24 <vivek.kumar.cse20@itbhu.ac.in>
2022-10-17 01:49:51 +0530

    authentication added (create API endpoints and  Implement logout)

--- src/main/java/com/eightnote/nirvana/NirvanaApplication.java
+++ src/main/java/com/eightnote/nirvana/NirvanaApplication.java
@@ -1,14 +1,17 @@
 package com.eightnote.nirvana;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.Authenticator;
 
 @SpringBootApplication
 @ComponentScan({
@@ -21,7 +24,9 @@ import java.net.Authenticator;
 })
 @RestController
 public class NirvanaApplication {
-
+	@Autowired
+	JdbcUserDetailsManager userDetailsManager;
+	BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 	public static void main(String[] args) {
 		SpringApplication.run(NirvanaApplication.class, args);
 	}
@@ -34,6 +39,31 @@ public class NirvanaApplication {
 		System.out.println(authentication.getAuthorities().toString());
 		System.out.println(authentication.getDetails().toString());
 		System.out.println(authentication.getPrincipal().toString());
-		return "Hello";
+		return "Hello user %s with roles %s".formatted(authentication.getName(), authentication.getAuthorities());
+	}
+
+//	@GetMapping("/logout")
+//	public String logout() {
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		authentication.setAuthenticated(false);
+//		System.out.println(authentication.isAuthenticated());
+//		return "redirect:home";
+//	}
+
+	@GetMapping("/user")
+	public String s(
+			@RequestParam("name") String username,
+			@RequestParam("pass") String password,
+			@RequestParam("role") String role
+	) {
+		var user = User.withUsername(username)
+				.password(bCryptPasswordEncoder.encode(password))
+				.roles("ARTIST")
+				.authorities(role)
+				.build();
+
+		userDetailsManager.createUser(user);
+
+		return "Created user %s with roles %s".formatted(user.getUsername(), user.getAuthorities());
 	}
 }
---
+++ src/main/java/com/eightnote/nirvana/login/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.eightnote.nirvana.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jdbcUserDetailsManager()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/logout").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(dataSource);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
